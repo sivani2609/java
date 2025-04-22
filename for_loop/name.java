bc69c0d763d2a5c9a6e10f3bb8f3ff44f6e71eae
sivani2609 <vbsivani.sep26@gmail.com>
2025-04-22 23:04:13 +0530

    Add files via upload

---
+++ for_loop/name.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class name{
+    public static void main(String[] args) {
+        System.out.println("enter the number of times to be printed:");
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        for(int i=0;i<num;i++){
+            System.out.println("sivani");
+        }
+
+
+    }
+}
---
+++ for_loop/number.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+class number{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        for(int i=1;i<11;i++){
+            System.out.println(i);
+        }
+    }
+}
