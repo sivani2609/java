42b78bc567a963a7a86372a119b9f0e8ddd5c56c
sivani2609 <vbsivani.sep26@gmail.com>
2025-05-19 23:57:28 +0530

    Add files via upload

---
+++ sort_array.java
@@ -0,0 +1,28 @@
+import  java.util.Scanner;
+class sort_array{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the number of elements:");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i =0;i<n;i++){
+            System.out.println("enter the number:");
+            arr[i] = sc.nextInt();
+        }
+        int temp;
+        for (int i = 0; i < arr.length; i++) { 
+           for (int j = i+1; j < arr.length; j++) { 
+            if(arr[i] < arr[j]) { 
+                 temp = arr[i]; 
+                 arr[i] = arr[j]; 
+                 arr[j] = temp; 
+              } 
+           } 
+       
+        for (i = 0; i < arr.length; i++) { 
+     System.out.print(arr[i] + " "); 
+   } 
+  } 
+}
+}        
+    
