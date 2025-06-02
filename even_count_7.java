94fc3f67ed4f3a1d96ad957213addf9f2914500e
sivani2609 <vbsivani.sep26@gmail.com>
2025-06-02 23:32:51 +0530

    Add files via upload

---
+++ even_count_7.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class even_count_7{
+    public static void main(String[] args){
+        int count=0;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter 5 numbers:");
+        for(int i =0;i<5;i++){
+            int num = sc.nextInt();
+            if(num%2 == 0){
+                count++;
+            }
+    }
+    System.out.println(count);
+        }
+    
+}
