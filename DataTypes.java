f74e81d2cfe4cf25c52576b2a2cb99422d6b5e50
sivani2609 <vbsivani.sep26@gmail.com>
2025-06-04 23:20:19 +0530

    Create DataTypes.java

---
+++ DataTypes.java
@@ -0,0 +1,12 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        int number = 10;
+        double decimal = 3.14;
+        String text = "Hello";
+        boolean flag = true;
+        System.out.println("Integer: " + number);
+        System.out.println("Double: " + decimal);
+        System.out.println("String: " + text);
+        System.out.println("Boolean: " + flag);
+    }
+}
