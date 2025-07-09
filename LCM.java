a1ab0866af360cfb2f22479c02c1a6c7fe5bddc4
sivani2609 <vbsivani.sep26@gmail.com>
2025-07-09 22:40:17 +0530

    Create LCM.java

---
+++ LCM.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main {
+  public static void main(String[] args) {
+    int n1 = 72, n2 = 120;
+    int gcd = findGCD(n1, n2);
+    int lcm = (n1 * n2) / gcd;
+    System.out.printf("The LCM of %d and %d is %d.", n1, n2, lcm);
+  }
+  public static int findGCD(int a, int b) {
+    if (b == 0)
+      return a;
+    return findGCD(b, a % b);
+  }
+}
