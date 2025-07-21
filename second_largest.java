a285a30e78696a1cf4c4737fe5f0c1a6cadaecc8
sivani2609 <vbsivani.sep26@gmail.com>
2025-07-21 22:39:43 +0530

    Create second_largest.java

---
+++ second_largest.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+class second_largest{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i=0;i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+        int temp;
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if(arr[i] > arr[j]){
+                    temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        int second_last = arr[n-2];
+        System.out.println("second largest number is:"+second_last);
+    }
+}
