781ff953fe905df650ca7e57434604f197f9230f
sivani2609 <vbsivani.sep26@gmail.com>
2025-07-31 21:31:58 +0530

    Create StringComparison.java

---
+++ StringComparison.java
@@ -0,0 +1,9 @@
+public class StringComparison {
+    public static void main(String[] args) {
+        String str1 = "Hello";
+        String str2 = "Hello";
+        String str3 = "World";
+        System.out.println(str1.equals(str2)); // true
+        System.out.println(str1.equals(str3)); // false
+    }
+}
