5bd8ba060e1a506845e5f0880a8378c55b93bd92
sivani2609 <vbsivani.sep26@gmail.com>
2025-08-01 22:57:30 +0530

    Create time.java

---
+++ time.java
@@ -0,0 +1,9 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+public class TimeString {
+    public static void main(String[] args) {
+        String string = "2017-07-25";
+        LocalDate date = LocalDate.parse(string, DateTimeFormatter.ISO_DATE);
+        System.out.println(date);
+    }
+}
