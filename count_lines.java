a17f8eaca6a2df147e146d8f37019d3432a84bd6
sivani2609 <vbsivani.sep26@gmail.com>
2025-08-02 23:55:49 +0530

    Create count_lines.java

---
+++ count_lines.java
@@ -0,0 +1,19 @@
+import java.io.File;
+import java.util.Scanner;
+class Main {
+  public static void main(String[] args) {
+    int count = 0;
+    try {
+      File file = new File("input.txt");
+      Scanner sc = new Scanner(file);
+      while(sc.hasNextLine()) {
+        sc.nextLine();
+        count++;
+      }
+      System.out.println("Total Number of Lines: " + count);
+      sc.close();
+    } catch (Exception e) {
+      e.getStackTrace();
+    }
+  }
+}
