a18480e3efacc2d2ec4f5228c6856fe3b65e7adf
sivani2609 <vbsivani.sep26@gmail.com>
2025-10-11 23:37:29 +0530

    Create rank_replacement.java

---
+++ rank_replacement.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+class rank_replacement{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] arr= new int[n];
+        int[] rank =new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        for(int i=0;i<n;i++){
+            int r=1;
+            for(int j=0;j<n;j++){
+            if(arr[i] <arr[j]){
+                r++;
+                }
+            }
+            rank[i]=r;
+        }
+        for(int i=0;i<n;i++){
+            System.out.println(rank[i]+" ");
+        }
+    }
+}
